5a1e9c6494fc0276a65758e02f159d0c86bc67ed
Jacob Neal <jneal26@msudenver.edu>
2019-05-10 22:12:16 -0600

    late

--- main.py
+++ main.py
@@ -1,24 +1,68 @@
 from stack import Stack
-
+class arrList():
 
 def test_integers():
-    s = Stack()
+    s = Stack(3)
     s.push(5)
     t = s.pop()
     print(t)
 
 
 def test_floats():
-    pass
+    s = Stack()
+    s.push(2,3)
+    t = s.pop()
+    print(t)
 
 
 def test_strings():
-    pass
+    s = Stack("")
+    s.push("Ahh")
+    t = s.pop()
+    print(t)
 
 
-def test_all():
-    pass
+def test_exceptions():
+    s = Stack(5)
+    s.push("ahh")
+    t = s.pop()
+    print(t)
 
+def safe_test_int_excption():
+    s = InStack()
+    s.push(1.0)
 
 if __name__ == '__main__':
     test_integers()
+    try:
+        test_exceptions()
+
+    except ValueError:
+        print("test value error")
+
+
+    try:
+        test_strings()
+
+    except ValueError:
+        print("string error")
+
+    try:
+        test_integers()
+
+    except ValueError:
+        print("Int error")
+
+    try:
+        test_floats()
+
+    except ValueError:
+        print("float error")
+
+    try:
+        safe_test_int_excption()
+
+    except ValueError:
+        print("Value error")
+
+    
